7d727101849f9d025ee2195751d4de5eef497382
maayanMenashe <maayanreal@gmail.com>
2026-05-20 00:08:50 +0300

    made character class abstract

--- Character.cs
+++ Character.cs
@@ -1,6 +1,6 @@
 ﻿namespace Assignment2_Maayan_Menashe;
 
-public class Character // base class for all the characters
+public abstract class Character // base class for all the characters
 {
     #region Functions
 
